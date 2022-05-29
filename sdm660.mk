33a119363b655c14ab8c46e977af5e638c1944e4
biyaniyash23 <yash.biyani23@gmail.com>
2022-05-29 23:00:47 +0000

    whyred: Add Vulkan and EGL hardware variant properties

--- sdm660.mk
+++ sdm660.mk
@@ -174,6 +174,10 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     vendor.display.config@1.1
 
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.hardware.vulkan=adreno \
+ro.hardware.egl=adreno 
+
 # Display Device Config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
